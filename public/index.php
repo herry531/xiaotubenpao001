c0d7cb14f9759cbd02e879834823427502228b51
z2168465 <573775605@qq.com>
2018-11-14 17:28:48 +0800

    更新

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -52,6 +51,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
+
     $request = Illuminate\Http\Request::capture()
 );
 
